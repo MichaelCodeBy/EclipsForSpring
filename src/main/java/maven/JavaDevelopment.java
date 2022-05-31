f9d6aec85b4d0da1c8bfbdc8bd5cb0b3108c9a8c
MichaelCodeBy <gomellc@gmail.com>
2022-05-31 11:56:46 +0300

    start work spring on eclips

---
+++ src/main/java/maven/JavaDevelopment.java
@@ -0,0 +1,19 @@
+package maven;
+public class JavaDevelopment{
+	public String getName() {
+		return "Alex";
+	
+	}
+	public String getJob() {
+		return "Middle Java developer";
+	}
+}
+
+public class HiringDepartment{
+	private JavaDevelopment javaDevelopment;
+	
+	public HiringDepartment(JavaDevelopment javaDevelopment) {
+		this.javaDevelopment=javaDevelopment;
+	}
+	
+}
